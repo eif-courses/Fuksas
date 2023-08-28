ab6a9e62138de86cffb860d7ae71097a824ccb97
Marius <m.gzegozevskis@gmail.com>
2023-08-29 00:20:46 +0300

    Auth JWT custom API

--- app/src/main/java/eif/viko/lt/faculty/app/presentation/ui/gems/components/GemListItem.kt
+++ app/src/main/java/eif/viko/lt/faculty/app/presentation/ui/gems/components/GemListItem.kt
@@ -1,24 +1,33 @@
 package eif.viko.lt.faculty.app.presentation.ui.gems.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import eif.viko.lt.faculty.app.R
 import eif.viko.lt.faculty.app.data.remote.mappers.GemsDto
 
 @Composable
-fun GemListItem(modifier: Modifier = Modifier,
-             gems: List<GemsDto>,
-             itemTextStyle: TextStyle = TextStyle(fontSize = 18.sp),
-             onItemClick: (GemsDto) -> Unit) {
+fun GemListItem(
+    modifier: Modifier = Modifier,
+    gems: List<GemsDto>,
+    itemTextStyle: TextStyle = TextStyle(fontSize = 18.sp),
+    onItemClick: (GemsDto) -> Unit
+) {
     LazyColumn(modifier) {
         items(gems.size) { i ->
             Row(modifier = Modifier
@@ -33,6 +42,18 @@ fun GemListItem(modifier: Modifier = Modifier,
 //                    contentDescription = item.Name,
 //                )
                 //Spacer(modifier = Modifier.width(16.dp))
+                if (gems[i].gem?.gem_type.equals("DIAMOND")) {
+                    Image(
+                        painter = painterResource(id = R.drawable.diamond),
+                        contentDescription = "Kosmosas",modifier.width(50.dp)
+                    )
+                }else{
+                    Image(
+                        painter = painterResource(id = R.drawable.emerald),
+                        contentDescription = "Kosmosas",modifier.width(42.dp)
+                    )
+                }
+
                 gems[i].gem?.let {
                     Text(
                         text = it.gem_type,
